22ee80c9ade3de337ff19434f87d691a60d8ee55
ptgmm <ptgmm@proton.me>
2024-07-03 23:10:10 +0200

    unit converter (cm and inch)

---
+++ unitconverter/unitconverter.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main ()
+{
+    int option;
+    float cmtoinch;
+    float inchtocm;
+    printf("cm to inch (0)\n");
+    printf("inch to cm (1)\n");
+    printf("Enter your choice: ");
+    scanf("%d", &option);
+
+    if (option == 0) {
+        printf("cm: ");
+        scanf("%f", &cmtoinch);
+        printf("inch: %.1f", cmtoinch * 0.3937008);
+    }
+    else if (option == 1) {
+        printf("inch: ");
+        scanf("%f", &inchtocm);
+        printf("cm: %.1f", inchtocm * 2.54);
+    }
+    else {
+        printf("Error");
+    }
+
+
+
+
+
+
+
+}
