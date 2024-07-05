324594de45455badef8610b7f7bb9c7d384daa35
ptgmm <ptgmm@proton.me>
2024-07-06 00:22:53 +0200

    final touches

--- BMI/bmi.c
+++ BMI/bmi.c
@@ -6,12 +6,10 @@ int main()
     float worldweight;
     float americanheight;
     float americanweight;
-    float worldheightm2 = worldheight * worldheight;
-    float americanheightm2 = americanheight * americanheight;
     int choice;
 
     printf("Hello in BMI calculator\n");
-    printf("cm/kg(0) inch/lbs(1)\n");
+    printf("cm/kg(0) feet/lbs(1)\n");
     printf("Enter your choice: ");
     scanf("%d", &choice);
 
@@ -20,14 +18,18 @@ int main()
         scanf("%f", &worldheight);
         printf("Enter your weight: ");
         scanf("%f", &worldweight);
-        printf("Your BMI is: %f", worldweight / worldheightm2);
+        float worldheightm2 = (worldheight / 100.0) * (worldheight / 100.0);
+        printf("Your BMI is: %.1f\n", worldweight / worldheightm2);
     }
     else if (choice == 1) {
         printf("Enter your height: ");
         scanf("%f", &americanheight);
         printf("Enter your weight: ");
         scanf("%f", &americanweight);
-        printf("Your BMI is: %f", americanweight / americanheightm2);
+        float tocm =  americanheight * 0.3048;
+        float tokg =  americanweight * 0.453592;
+        float americanheightm2 = tocm * tocm;
+        printf("Your BMI is: %.1f\n", tokg / americanheightm2);
     }
     else {
         printf("You can choose only 0 or 1");
