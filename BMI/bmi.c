a4899eb8ed36a4449b024d087cea48dee0c648ca
ptgmm <ptgmm@proton.me>
2024-07-06 00:41:56 +0200

    Rename variables

--- BMI/bmi.c
+++ BMI/bmi.c
@@ -2,34 +2,34 @@
 
 int main()
 {
-    float worldheight;
-    float worldweight;
-    float americanheight;
-    float americanweight;
+    float cm;
+    float kg;
+    float feet;
+    float pounds;
     int choice;
 
     printf("Hello in BMI calculator\n");
-    printf("cm/kg(0) feet/lbs(1)\n");
+    printf("cm/kg(0) feet/pounds(1)\n");
     printf("Enter your choice: ");
     scanf("%d", &choice);
 
     if (choice == 0) {
         printf("Enter your height: ");
-        scanf("%f", &worldheight);
+        scanf("%f", &cm);
         printf("Enter your weight: ");
-        scanf("%f", &worldweight);
-        float worldheightm2 = (worldheight / 100.0) * (worldheight / 100.0);
-        printf("Your BMI is: %.1f\n", worldweight / worldheightm2);
+        scanf("%f", &kg);
+        float cmtom2 = cm * cm / 10000.0;
+        printf("Your BMI is: %.2f\n", kg / cmtom2);
     }
     else if (choice == 1) {
         printf("Enter your height: ");
-        scanf("%f", &americanheight);
+        scanf("%f", &feet);
         printf("Enter your weight: ");
-        scanf("%f", &americanweight);
-        float tocm =  americanheight * 0.3048;
-        float tokg =  americanweight * 0.453592;
-        float americanheightm2 = tocm * tocm;
-        printf("Your BMI is: %.1f\n", tokg / americanheightm2);
+        scanf("%f", &pounds);
+        float feettocm =  feet * 0.3048;
+        float poundstokg =  pounds * 0.453592;
+        float feettom2 = feettocm * feettocm;
+        printf("Your BMI is: %.2f\n", poundstokg / feettom2);
     }
     else {
         printf("You can choose only 0 or 1");
