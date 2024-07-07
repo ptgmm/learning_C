2e7427b33a5780e0b572957546e953c5f568c990
ptgmm <ptgmm@proton.me>
2024-07-07 23:23:04 +0200

    Delete previous todo. You can add task to file

--- todo/todo.c
+++ todo/todo.c
@@ -1,13 +1,19 @@
 #include <string.h>
 #include <stdio.h>
 int main()
-{
-    char task[20];
-    char list[10][20];
-    printf("Add task: \n");
-    fgets(task, sizeof(task), stdin);
-    strcpy(list[0], task);
-    printf("You add %s\n", list[0]);
+{   
+    FILE *fptr;
+    char taskadd[100];
+    fptr = fopen("task.txt", "w");
+   
+    printf("Add Task: ");
+    scanf("%99s", taskadd);
+
+    fprintf(fptr, "%s\n", taskadd);
+
+    fclose(fptr);
+
+    
     return 0;
 }
 
