182995415c645b6028c86064d7466e2dd9157aca
ptgmm <ptgmm@proton.me>
2024-08-22 21:10:22 +0200

    Error Handling

--- gradebook/gradebook.c
+++ gradebook/gradebook.c
@@ -8,7 +8,7 @@ int main()
     char filename[100];
     char command[100];
     int start;
-    char new[255];
+    char newfile[255];
 
     
     printf("Open gradebook (0)\n");
@@ -24,6 +24,9 @@ int main()
             if (access(filename, F_OK) == 0) {
                 snprintf(command, sizeof(command), "vim %s", filename);
                 int firstresult = system(command);
+                if (firstresult != 0) {
+                    printf("Error: Command failed with return code %d\n", firstresult); 
+                }
             }
             else {
                 printf("File does not exist.\n");
@@ -31,10 +34,19 @@ int main()
             break;
         case 1:
             printf("Enter a name of your new gradebook\n");
-            scanf("%99s", &new);
-            createfile = fopen(new, "w");
-            snprintf(command, sizeof(command), " vim %s", new);
-            int secondresult = system(command);
+            scanf("%99s", &newfile);
+            createfile = fopen(newfile, "w");
+            if (createfile == NULL) {
+                printf("Error: Could not create a file");
+            }
+            else {
+                snprintf(command, sizeof(command), "vim %s", newfile);
+                int secondresult = system(command);
+                fclose(createfile);
+                if (secondresult != 0) {
+                    printf("Error: Command failed with return code %d\n", secondresult); 
+                }
+            }
             break;
             
     }
